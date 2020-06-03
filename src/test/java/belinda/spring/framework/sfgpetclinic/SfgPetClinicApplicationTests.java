c8dd467c161f30cda9fd6d3d7ace00bf19511f4a
Belindavf <belibvf-17@hotmail.com>
2020-06-03 13:00:53 +0200

    Initial Project Add

---
+++ src/test/java/belinda/spring/framework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package belinda.spring.framework.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
